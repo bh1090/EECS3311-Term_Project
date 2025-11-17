b2a87fb47db9e9a8d03973111a99f55e08d1b7f5
George <george_zakko@hotmail.com>
2025-11-17 13:58:22 -0500

    Implemented the EditMaintenanceRequestService class, which was largely based on the SubmitServiceRequestService class. Design-wise, basically all the same reasons for designing SubmitServiceRequestService were applicable for designing EditMaintenanceRequestService.

--- src/main/java/Service/EditMaintenanceRequestService.java
+++ src/main/java/Service/EditMaintenanceRequestService.java
@@ -1,4 +1,27 @@
 package Service;
 
+import Model.EssentialServiceRequest;
+import Model.NonEssentialServiceRequest;
+import Model.ServiceRequest;
+import Repository.ServiceRequestRepository;
+
 public class EditMaintenanceRequestService {
+     final ServiceRequestRepository requestRepository;
+
+     public EditMaintenanceRequestService(ServiceRequestRepository requestRepository) {
+          this.requestRepository = requestRepository;
+     }
+     public ServiceRequest update(String roomID, String description, boolean isEssential){
+          if(roomID == null){
+               throw new IllegalArgumentException("No room ID.  ");
+          }
+          if(isEssential){
+               EssentialServiceRequest serviceRequest = new EssentialServiceRequest(0, description, true);
+               return requestRepository.update(serviceRequest);
+          }
+          else{
+               NonEssentialServiceRequest serviceRequest = new NonEssentialServiceRequest(0, description);
+               return requestRepository.update(serviceRequest);
+          }
+     }
 }

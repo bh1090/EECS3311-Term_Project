8c0721106b32d926e8bfb665ac55aa061e28dc33
George <george_zakko@hotmail.com>
2025-11-17 10:05:54 -0500

    Created the constructor for service requests.

--- src/main/java/Model/ServiceRequest.java
+++ src/main/java/Model/ServiceRequest.java
@@ -3,5 +3,9 @@ package Model;
 public abstract class ServiceRequest {
      private int serviceRequestID;
 
+     public ServiceRequest(int serviceRequestID) {
+          this.serviceRequestID = serviceRequestID;
+     }
+
      public abstract int getServiceRequestID();
 }

26a1d49e04dc497eaef4dbaae24ef41f659b317c
George <george_zakko@hotmail.com>
2025-11-17 12:53:15 -0500

    Simply just made the submission object for a service request, and made it part of the class constructor.

--- src/main/java/Controller/AddServiceRequestController.java
+++ src/main/java/Controller/AddServiceRequestController.java
@@ -3,4 +3,9 @@ package Controller;
 import Service.SubmitServiceRequestService;
 
 public class AddServiceRequestController {
+     final SubmitServiceRequestService submitServiceRequestService;
+
+     public AddServiceRequestController(SubmitServiceRequestService submitServiceRequestService) {
+          this.submitServiceRequestService = submitServiceRequestService;
+     }
 }

dd95541fbc8e169c340e69111300c36146d1593a
George <george_zakko@hotmail.com>
2025-11-17 11:58:07 -0500

    Added the description field to the service request parent class and it's constructor.

--- src/main/java/Model/ServiceRequest.java
+++ src/main/java/Model/ServiceRequest.java
@@ -2,9 +2,11 @@ package Model;
 
 public abstract class ServiceRequest {
      private int serviceRequestID;
+     private String description;
 
-     public ServiceRequest(int serviceRequestID) {
+     public ServiceRequest(int serviceRequestID, String description) {
           this.serviceRequestID = serviceRequestID;
+          this.description = description;
      }
 
      public int getServiceRequestID() {

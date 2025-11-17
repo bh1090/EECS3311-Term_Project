5e10e80d092c2ce903e9a6a4fffcf9a7f5328c30
George <george_zakko@hotmail.com>
2025-11-17 17:35:08 -0500

    A lot of this model code is reused from the AddServiceRequestView class.  Differences are that maintenance and admin both have access to this view, and maintenance can change the status of a service request.

--- src/main/java/View/ServiceRequestsListView.java
+++ src/main/java/View/ServiceRequestsListView.java
@@ -1,4 +1,27 @@
 package View;
 
 public class ServiceRequestsListView {
+     final MaintenanceServiceRequestController maintenanceServiceRequestController;
+     final AdminServiceRequestController adminServiceRequestController;
+
+     public ServiceRequestsListView(MaintenanceServiceRequestController maintenanceServiceRequestController, AdminServiceRequestController adminServiceRequestController) {
+          this.maintenanceServiceRequestController = maintenanceServiceRequestController;
+          this.adminServiceRequestController = adminServiceRequestController;
+     }
+
+     public void getMaintenanceUserInput(){
+          Scanner sc = new Scanner(System.in);
+
+          System.out.print("Select service request: ");
+          // Some code that grabs both essential and non-essential
+          // list of service requests from a given room.  For now, use placeholder.
+          ServiceRequest serviceRequest = null;
+          System.out.print("Enter the status update: ");
+          String statusUpdate = sc.nextLine();
+
+          System.out.print("Enter whether or not it is essential:");
+          boolean isEssential = sc.nextBoolean();
+
+          maintenanceServiceRequestController.handleServiceRequestModification(serviceRequest, isEssential);
+     }
 }

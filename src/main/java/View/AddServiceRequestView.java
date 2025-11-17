9ed2b4cf9b7bf8a63cc2bb6fb5bd3d1b431b0c39
George <george_zakko@hotmail.com>
2025-11-17 13:20:00 -0500

    Implemented a kind of placeholder method for getting the user input, and sending it off to the AddServiceRequestController class. It's a placeholder because I'm not sure how we're going to  handle the front-end yet, so I just have print statements and scanner statements to get input from a user.

--- src/main/java/View/AddServiceRequestView.java
+++ src/main/java/View/AddServiceRequestView.java
@@ -8,5 +8,18 @@ public class AddServiceRequestView {
      public AddServiceRequestView(AddServiceRequestController serviceRequestController) {
           this.serviceRequestController = serviceRequestController;
      }
-     
+     public void getUserInput(){
+          Scanner sc = new Scanner(System.in);
+
+          System.out.print("Enter the room ID: ");
+          String roomID = sc.nextLine();
+
+          System.out.print("Enter the description of the service request: ");
+          String description = sc.nextLine();
+
+          System.out.print("Enter whether or not it is essential:");
+          boolean isEssential = sc.nextBoolean();
+
+          serviceRequestController.handleServiceRequestSubmission(roomID, description, isEssential);
+     }
 }

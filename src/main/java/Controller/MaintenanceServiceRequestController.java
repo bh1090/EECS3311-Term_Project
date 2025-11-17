7c6fb5c1895df713480fff538aff53851f9cb709
George <george_zakko@hotmail.com>
2025-11-17 13:29:20 -0500

    Create an object out of the EditMaintenanceRequestService class, which gets initialized by the class constructor.

--- src/main/java/Controller/MaintenanceServiceRequestController.java
+++ src/main/java/Controller/MaintenanceServiceRequestController.java
@@ -3,4 +3,9 @@ package Controller;
 import Service.EditMaintenanceRequestService;
 
 public class MaintenanceServiceRequestController {
+     final EditMaintenanceRequestService editMaintenanceRequestService;
+
+     public MaintenanceServiceRequestController(EditMaintenanceRequestService editMaintenanceRequestService) {
+          this.editMaintenanceRequestService = editMaintenanceRequestService;
+     }
 }

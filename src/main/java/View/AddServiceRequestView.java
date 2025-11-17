ba518f48cf6b84170dbbe36b9c7296ff10face1f
George <george_zakko@hotmail.com>
2025-11-17 13:13:18 -0500

    Created an object out of the AddServiceRequestController class. Also initialized the object through the class constructor. This is because we need to use the handleServiceRequestSubmission from the object.

--- src/main/java/View/AddServiceRequestView.java
+++ src/main/java/View/AddServiceRequestView.java
@@ -3,4 +3,10 @@ package View;
 import Controller.AddServiceRequestController;
 
 public class AddServiceRequestView {
+     final AddServiceRequestController serviceRequestController;
+
+     public AddServiceRequestView(AddServiceRequestController serviceRequestController) {
+          this.serviceRequestController = serviceRequestController;
+     }
+     
 }

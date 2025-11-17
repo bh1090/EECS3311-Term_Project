537606ff1fc6c33339da000b2de7c93253c812c3
George <george_zakko@hotmail.com>
2025-11-17 13:05:44 -0500

    Added an import for service request, so I could make an object of it to submit.
    Changed naming of variable submitServiceRequest; previously ended with 'Service' which was unintended.
    
    Finally, I implemented the handleServiceRequestSubmission method which creates an object of the ServiceRequest class in order to submit a service request.  This object is handled by the SubmitServiceRequestService class, which determines if it essential or non-essential, or if it's even a proper object.

--- src/main/java/Controller/AddServiceRequestController.java
+++ src/main/java/Controller/AddServiceRequestController.java
@@ -1,11 +1,16 @@
 package Controller;
 
+import Model.ServiceRequest;
 import Service.SubmitServiceRequestService;
 
 public class AddServiceRequestController {
-     final SubmitServiceRequestService submitServiceRequestService;
+     final SubmitServiceRequestService submitServiceRequest;
 
-     public AddServiceRequestController(SubmitServiceRequestService submitServiceRequestService) {
-          this.submitServiceRequestService = submitServiceRequestService;
+     public AddServiceRequestController(SubmitServiceRequestService submitServiceRequest) {
+          this.submitServiceRequest = submitServiceRequest;
+     }
+
+     public void handleServiceRequestSubmission(String roomID, String description, boolean isEssential){
+          ServiceRequest sr = submitServiceRequest.submit(roomID, description, isEssential);
      }
 }

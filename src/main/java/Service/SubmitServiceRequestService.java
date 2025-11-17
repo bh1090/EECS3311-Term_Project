6f2cd6e844d0592a1a6f5bdf255033e7764f2044
George <george_zakko@hotmail.com>
2025-11-17 12:28:30 -0500

    Created the submit method. It checks to see if the room ID or description is null because the admin may've incorrectly submitted something. It creates a request object depending on whether the admin decided the submission of a request was essential or non-essential.

--- src/main/java/Service/SubmitServiceRequestService.java
+++ src/main/java/Service/SubmitServiceRequestService.java
@@ -5,4 +5,15 @@ import Model.NonEssentialServiceRequest;
 import Model.ServiceRequest;
 
 public class SubmitServiceRequestService {
+     public ServiceRequest submit(String roomID, String description, boolean isEssential){
+         if(roomID == null || description == null){
+              throw new IllegalArgumentException("No room ID and/or no description.  ");
+         }
+          if(isEssential){
+               return new EssentialServiceRequest(0, description, false);
+         }
+          else{
+               return new NonEssentialServiceRequest(0, description);
+          }
+     }
 }

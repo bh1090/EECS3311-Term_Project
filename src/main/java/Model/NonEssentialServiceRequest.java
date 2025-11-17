704aacdcee9a25d74ba976a5f75917fee3e34866
George <george_zakko@hotmail.com>
2025-11-17 10:42:41 -0500

    Added the basic body of method for promoting non-essential service requests to the essential service request arraylist that will be in a room object.

--- src/main/java/Model/NonEssentialServiceRequest.java
+++ src/main/java/Model/NonEssentialServiceRequest.java
@@ -10,5 +10,9 @@ public class NonEssentialServiceRequest extends ServiceRequest{
      public int getServiceRequestID(){
           return super.getServiceRequestID();
      }
+     public void promoteServiceRequest(NonEssentialServiceRequest nesr){
+          //To-do: make so nesr gets added to a specific rooms arraylist of
+          // essential service requests.  
+     }
 
 }

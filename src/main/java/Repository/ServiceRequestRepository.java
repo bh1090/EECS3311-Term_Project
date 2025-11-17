5aee113f3a6f42f9c6496febf206d0000197e957
George <george_zakko@hotmail.com>
2025-11-17 12:42:55 -0500

    Just added a basic placeholder save method in the service request repo until I properly implement it.

--- src/main/java/Repository/ServiceRequestRepository.java
+++ src/main/java/Repository/ServiceRequestRepository.java
@@ -1,4 +1,10 @@
 package Repository;
 
+import Model.EssentialServiceRequest;
+import Model.ServiceRequest;
+
 public class ServiceRequestRepository {
+     public ServiceRequest save(ServiceRequest serviceRequest) {
+          return null;
+     }
 }

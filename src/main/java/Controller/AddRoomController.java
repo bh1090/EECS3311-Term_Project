f460539186cd53ef1d0da1c267485426e27e86fe
George <george_zakko@hotmail.com>
2025-11-17 20:35:06 -0500

    A lot of my code is commented out because it is dependent on other group member's code that isn't available yet. Same setup for any controller class, where we created a class object of the roomService object and initialize it through the class constructor. A new room object is created in the handleAddRoomSubmission with the corresponding inputs of the admin.

--- src/main/java/Controller/AddRoomController.java
+++ src/main/java/Controller/AddRoomController.java
@@ -1,4 +1,11 @@
 package Controller;
 
 public class AddRoomController {
+//     final RoomService roomService;
+//     public AddRoomController(RoomService roomService){
+//          this.roomService = roomService;
+//     }
+     public void handleAddRoomSubmission(String roomName, int capacity, String location){
+          //Room room = submitServiceRequestService.submit(roomID, capacity, location);
+     }
 }

d617772d9fdfe7452eb5cd5721a4e211c65a1f6b
George <george_zakko@hotmail.com>
2025-11-17 19:56:13 -0500

    Class constructor initializes the AddRoomController object.

--- src/main/java/View/AddRoomView.java
+++ src/main/java/View/AddRoomView.java
@@ -3,4 +3,9 @@ package View;
 import Controller.AddRoomController;
 
 public class AddRoomView {
+     final AddRoomController addRoomController;
+
+     public AddRoomView(AddRoomController addRoomController) {
+          this.addRoomController = addRoomController;
+     }
 }

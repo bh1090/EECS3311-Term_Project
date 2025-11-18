2903a349dc19b8536ffa5e2ef1173579a4b09da3
George <george_zakko@hotmail.com>
2025-11-17 20:22:28 -0500

    Added a function that gets the admin input for when they want to add room for inputs that aren't automatically created.

--- src/main/java/View/AddRoomView.java
+++ src/main/java/View/AddRoomView.java
@@ -8,4 +8,18 @@ public class AddRoomView {
      public AddRoomView(AddRoomController addRoomController) {
           this.addRoomController = addRoomController;
      }
+     public void getAdminAddRoomInput(){
+          Scanner sc = new Scanner(System.in);
+
+          System.out.print("Please enter the new room name: ");
+          String roomName = sc.nextLine();
+
+          System.out.print("Please specify the new room's capacity: ");
+          int capacity = sc.nextInt();
+
+          System.out.print("Please enter the location of the new room: ");
+          String location = sc.nextLine();
+
+          addRoomController.handleAddRoomSubmission(roomName, capacity, location);
+     }
 }

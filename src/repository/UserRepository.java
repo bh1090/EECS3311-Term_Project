d7bca01e971c974f55c0d7f21e3724983dbef4f0
peter <peterdro39@gmail.com>
2025-11-21 17:19:48 -0500

    updated class

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,5 +1,43 @@
 package repository;
 
+import model.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserRepository {
 
+    private static UserRepository singletonInstance= new UserRepository();
+
+    private final Map<String, User> usersByEmail= new HashMap<>();
+
+    private UserRepository() { 
+    	
+    }
+
+    public static UserRepository getInstance() {
+        return singletonInstance;
+    }
+
+    
+    public void save(User user) {
+        if (user== null || user.getEmail()== null) {
+        	
+            return;
+        }
+        
+        usersByEmail.put(user.getEmail().toLowerCase(), user);
+    }
+
+   
+    //returns the email of the user
+    public User findByEmail(String email) {
+    	
+        if (email==null) {
+        	
+        	return null;
+        }
+        
+        return usersByEmail.get(email.toLowerCase());
+        
+    }
 }

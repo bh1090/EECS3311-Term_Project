5f95fb74092ca5a77cf87b7fdc92d15b0ae46e33
peter <peterdro39@gmail.com>
2025-11-21 17:20:52 -0500

    UserService class updated

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,27 @@
 package service;
 
+import database.*;
+import model.*;
+import repository.*;
+
+
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService() {
+    	
+        this.userRepository= Database.getInstance().getUserRepository();
+    }
+
+    
+    public User createUser(AccountType accountType,String name,String email,String password) {
+    	User user= UserFactory.createUser(accountType, name, email, password);
+    	
+        userRepository.save(user);
+        
+        return user;
+    }
+
+   
 }

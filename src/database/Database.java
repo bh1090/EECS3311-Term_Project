9f5f03484b51cd571627dc0ab465c40c76d4add8
peter <peterdro39@gmail.com>
2025-11-21 17:25:25 -0500

    updated database class

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,5 +1,24 @@
 package database;
 
+import repository.*;
+
 public class Database {
 
+    private static Database singletonInstance= new Database();
+
+    private  UserRepository userRepository= UserRepository.getInstance();
+
+    private Database() { 
+    	
+    }
+
+    public static Database getInstance() {
+    	
+        return singletonInstance;
+    }
+
+    public UserRepository getUserRepository() {
+        return  userRepository;
+    }
+    
 }

ded9ba4cbd6b3f54ff6b82f43081b09894039914
George <george_zakko@hotmail.com>
2025-11-22 15:55:20 -0500

    Took a lot of the code from the AdminSelectActionView and reformatted it to reflect the chief event coordinators actions like removing and adding admins.

--- src/main/java/View/ChiefEventCoordinatorView.java
+++ src/main/java/View/ChiefEventCoordinatorView.java
@@ -1,4 +1,41 @@
 package View;
 
 public class ChiefEventCoordinatorView {
+     final ChiefEventCoordinatorController chiefEventCoordinatorController = new ChiefEventCoordinatorController();
+
+     public void handleChiefCoordinatorAction() {
+          JFrame jframe = new JFrame();
+
+          JButton button1 = new JButton("Create Admin");
+          JButton button2 = new JButton("Remove Admin");
+
+          button1.setBounds(150, 200, 220, 50);
+          jframe.add(button1);
+          button2.setBounds(150, 250, 220, 50);
+          jframe.add(button2);
+          jframe.setSize(500, 600);
+          jframe.setLayout(null);
+          jframe.setVisible(true);
+          button1.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                    jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set default close operation
+                    jframe.setSize(300, 200);
+                    jframe.setLocationRelativeTo(null);
+                    jframe.dispose();
+
+                    chiefEventCoordinatorController.createAdmin();
+
+               }
+          });
+          button2.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                    jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set default close operation
+                    jframe.setSize(300, 200);
+                    jframe.setLocationRelativeTo(null);
+                    jframe.dispose();
+
+                    chiefEventCoordinatorController.removeAdmin();
+               }
+          });
+     }
 }

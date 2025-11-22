a6549d0a15a41d559704f45e7fa2edfac017b9ca
George <george_zakko@hotmail.com>
2025-11-22 15:56:18 -0500

    ChiefEventCoordinatorController and action listener imports are both necessary to make objects out of to use their corresponding methods.

--- src/main/java/View/ChiefEventCoordinatorView.java
+++ src/main/java/View/ChiefEventCoordinatorView.java
@@ -1,5 +1,11 @@
 package View;
 
+import Controller.ChiefEventCoordinatorController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ChiefEventCoordinatorView {
      final ChiefEventCoordinatorController chiefEventCoordinatorController = new ChiefEventCoordinatorController();
 

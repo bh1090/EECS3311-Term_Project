45d38dcc6a06deaeda7ea3ed69a4bfd8ab22dac3
George <george_zakko@hotmail.com>
2025-11-22 13:08:56 -0500

    Added the controller object for the rooms list to take control over from the rooms list view. Also added the class constructor for a rooms list controller. Implemented the getListOfRooms method which will have two buttons, view rooms and close, where if the admin clicks on the view rooms button, the getAllRooms method of the RoomsListController class is called, returning all the rooms in the system.  If the admin selects close, then the program simply ends.

--- src/main/java/View/RoomsListView.java
+++ src/main/java/View/RoomsListView.java
@@ -1,4 +1,40 @@
 package View;
 
 public class RoomsListView {
+     final RoomsListController roomsListController;
+
+     public RoomsListView(RoomsListController roomsListController) {
+          this.roomsListController = roomsListController;
+     }
+
+     public void getListOfRooms() {
+          JFrame jframe = new JFrame();
+          JButton button1 = new JButton("View Rooms");
+          button1.setBounds(150, 200, 220, 50);
+          jframe.add(button1);
+          JButton button2 = new JButton("Close");
+          button2.setBounds(150, 400, 220, 50);
+          jframe.add(button2);
+          jframe.setSize(500, 600);
+          jframe.setLayout(null);
+          jframe.setVisible(true);
+          button1.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e){
+                    jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set default close operation
+                    jframe.setSize(300, 200);
+                    jframe.setLocationRelativeTo(null);
+                    jframe.dispose();
+
+                    roomsListController.getAllRooms();
+               }
+          });
+          button2.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e){
+                    jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set default close operation
+                    jframe.setSize(300, 200);
+                    jframe.setLocationRelativeTo(null);
+                    jframe.dispose();
+               }
+          });
+     }
 }

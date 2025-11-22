bc3f649a5ae3f1c985c53d3cb9096de8f57aa608
George <george_zakko@hotmail.com>
2025-11-22 18:50:38 -0500

    The chief event coordinator extends the StaffUser class because it is one of the three subtypes of the staff users along with maintenance and admins. Just a simple call to the super constructor.

--- src/main/java/Model/EventCoordinator.java
+++ src/main/java/Model/EventCoordinator.java
@@ -1,4 +1,7 @@
 package Model;
 
-public class EventCoordinator {
-}
+//public class EventCoordinator extends StaffUser{
+//     public StaffUser(String name, String email, String password) {
+//          super(name, email, password,AccountType.STAFF);
+//     }
+//}

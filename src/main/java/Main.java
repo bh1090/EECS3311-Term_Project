5185dfd0d826d445624b068331ff088f9b7a6f43
George <george_zakko@hotmail.com>
2025-11-22 10:50:46 -0500

    Added a main method to test out GUI and the views.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import View.AdminSelectActionView;
+
+public class Main {
+     public static void main(String[] args){
+          AdminSelectActionView ascv = new AdminSelectActionView();
+          ascv.handleAdminAction();
+     }
+}

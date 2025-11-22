2a0a509ec12732946501cd63386ea5fddb88e2d0
George <george_zakko@hotmail.com>
2025-11-22 10:03:11 -0500

    Added some basic buttons for some of the actions the admin will have like adding a new room or view the list of rooms.

--- src/main/java/View/AdminSelectActionView.java
+++ src/main/java/View/AdminSelectActionView.java
@@ -3,4 +3,19 @@ package View;
 import javax.swing.*;
 
 public class AdminSelectActionView {
+     public void handleAdminAction(){
+          JFrame jframe = new JFrame();
+
+          JButton button1 = new JButton("Add New Room");
+          JButton button2 = new JButton("List Rooms");
+
+          button1.setBounds(150, 200, 220, 50);
+          jframe.add(button1);
+          button2.setBounds(150, 200, 220, 50);
+          jframe.add(button2);
+          jframe.setSize(500, 600);
+          jframe.setLayout(null);
+          jframe.setVisible(true);
+     }
+
 }

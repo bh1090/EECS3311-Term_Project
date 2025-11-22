fbfaadbf17f4704ffb0f2366ac6ef5d5ac589fee
George <george_zakko@hotmail.com>
2025-11-22 18:18:53 -0500

    Added the chief event coordinator view here just to test it and see if it works.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
      public static void main(String[] args){
           AdminSelectActionView adminSelectActionView = new AdminSelectActionView();
           adminSelectActionView.handleAdminAction();
+
+//          ChiefEventCoordinatorView chiefEventCoordinatorView = new ChiefEventCoordinatorView();
+//          chiefEventCoordinatorView.handleChiefCoordinatorAction();
      }
 }

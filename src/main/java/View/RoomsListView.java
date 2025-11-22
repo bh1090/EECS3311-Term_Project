06261dd4ad3e0ea273be1796152c4cb5df271c0f
George <george_zakko@hotmail.com>
2025-11-22 13:09:48 -0500

    RoomsListController import is necessary to create an object out RoomsListController to call it's getAllRooms method.

--- src/main/java/View/RoomsListView.java
+++ src/main/java/View/RoomsListView.java
@@ -1,5 +1,11 @@
 package View;
 
+import Controller.RoomsListController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RoomsListView {
      final RoomsListController roomsListController;
 

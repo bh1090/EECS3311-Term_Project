1edc767d9b3b37fe6fef88c47338dadb7facd8fb
George <george_zakko@hotmail.com>
2025-11-22 12:36:09 -0500

    Implemented two methods so far that just call upon the corresponding view the admin wants.

--- src/main/java/Controller/AdminSelectActionController.java
+++ src/main/java/Controller/AdminSelectActionController.java
@@ -1,4 +1,15 @@
 package Controller;
 
 public class AdminSelectActionController {
+
+     public void accessAddRoomView() {
+          AddRoomController addRoomController = new AddRoomController();
+          AddRoomView addRoomView = new AddRoomView(addRoomController);
+          addRoomView.getAdminAddRoomInput();
+     }
+     public void accessRoomsListView() {
+          AddRoomController addRoomController = new AddRoomController();
+          AddRoomView addRoomView = new AddRoomView(addRoomController);
+          addRoomView.getAdminAddRoomInput();
+     }
 }

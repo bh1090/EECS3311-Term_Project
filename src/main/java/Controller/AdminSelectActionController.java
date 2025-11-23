11141efe247a6bdcd7c36edbdf88d62dbf82d129
George <george_zakko@hotmail.com>
2025-11-22 21:23:59 -0500

    Using class constructor now to properly instantiate the two kinds of potential controller objects.

--- src/main/java/Controller/AdminSelectActionController.java
+++ src/main/java/Controller/AdminSelectActionController.java
@@ -4,8 +4,12 @@ import View.AddRoomView;
 import View.RoomsListView;
 
 public class AdminSelectActionController {
-     private final AddRoomController addRoomController = new AddRoomController();
-     private final RoomsListController roomsListController = new RoomsListController();
+     private final AddRoomController addRoomController;
+     private final RoomsListController roomsListController;
+     public AdminSelectActionController(AddRoomController addRoomController, RoomsListController roomsListController) {
+          this.addRoomController = addRoomController;
+          this.roomsListController = roomsListController;
+     }
 
      public void accessAddRoomView() {
           AddRoomView addRoomView = new AddRoomView(addRoomController);

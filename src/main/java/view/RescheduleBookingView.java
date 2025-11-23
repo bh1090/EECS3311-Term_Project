646e98e94872c1891b4cc15a9b35ce8036fff39b
Oscar Zhou <racso101@my.yorku.ca>
2025-11-23 17:39:57 -0500

    capitalization

--- src/main/java/view/RescheduleBookingView.java
+++ src/main/java/view/RescheduleBookingView.java
@@ -6,13 +6,13 @@ import application.SessionData;
 import java.awt.*;
 
 public class RescheduleBookingView extends JFrame{
-	public String UserID;
+	public String userID;
 	public JTextField bookingIDTextField = new JTextField(1);
 	public JButton backButton = new JButton("Back");
 	public JButton rescheduleBookingButton = new JButton("Reschedule Booking");
 	
 	public RescheduleBookingView() {
-		this.UserID = SessionData.getCurrentUser().getId();
+		this.userID = SessionData.getCurrentUser().getId();
 		
 		setTitle("Reschedule Booking");
 		setLocationRelativeTo(null);
@@ -20,7 +20,7 @@ public class RescheduleBookingView extends JFrame{
         
         setLayout(new GridLayout(3,2));
         add(new JLabel("User ID:"));
-        add(new JLabel(this.UserID));
+        add(new JLabel(this.userID));
         add(new JLabel("Booking ID:"));
         add(this.bookingIDTextField);
         add(this.backButton);

034c4bfcaf391d2e3d088cf112e04a24a5e9c5b2
George <george_zakko@hotmail.com>
2025-11-22 22:38:06 -0500

    Added private modifier.

--- src/main/java/View/AddRoomView.java
+++ src/main/java/View/AddRoomView.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddRoomView {
-     final AddRoomController addRoomController;
+     private final AddRoomController addRoomController;
 
      public AddRoomView(AddRoomController addRoomController) {
           this.addRoomController = addRoomController;

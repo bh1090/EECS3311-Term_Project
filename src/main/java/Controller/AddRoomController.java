581aeda7fb72fb4ece50888793dbe01e2d3a4022
George <george_zakko@hotmail.com>
2025-11-23 01:21:46 -0500

    Uncommented all code now that all classes needed are available.

--- src/main/java/Controller/AddRoomController.java
+++ src/main/java/Controller/AddRoomController.java
@@ -3,11 +3,11 @@ package Controller;
 import core.service.RoomService;
 
 public class AddRoomController {
-//     final RoomService roomService;
-//     public AddRoomController(RoomService roomService){
-//          this.roomService = roomService;
-//     }
-     public void handleAddRoomSubmission(String location, int capacity, double price){
-          // roomService.createRoom(location, capacity, price);
+     final RoomService roomService;
+     public AddRoomController(RoomService roomService){
+          this.roomService = roomService;
+     }
+     public void handleAddRoomSubmission(String location, int capacity, String price){
+          roomService.createRoom(location, capacity, price);
      }
 }

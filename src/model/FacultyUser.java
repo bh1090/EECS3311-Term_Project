fdb67039800efc2016ee8a375ef6cfb22d74c6ae
peter <peterdro39@gmail.com>
2025-11-22 22:21:25 -0500

    updated

--- src/model/FacultyUser.java
+++ src/model/FacultyUser.java
@@ -8,7 +8,7 @@ public class FacultyUser extends User {
     }
     
     //when CSV loading
-    public FacultyUser(String id,String name,String email, String password,boolean verified) {
-    	super(id,name, email,password,AccountType.FACULTY, verified);
+    public FacultyUser(String id, String name,String email,String password,boolean verified,String role) {
+        super(id,name, email, password,AccountType.FACULTY,verified, role);
     }
 }

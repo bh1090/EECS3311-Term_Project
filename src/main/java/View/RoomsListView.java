3b66f82ff8eec1b0d7cc47f900aaac2cf7992c9a
George <george_zakko@hotmail.com>
2025-11-22 21:01:16 -0500

    Removed redundant frame setting properties.

--- src/main/java/View/RoomsListView.java
+++ src/main/java/View/RoomsListView.java
@@ -26,9 +26,6 @@ public class RoomsListView {
           jframe.setVisible(true);
           button1.addActionListener(new ActionListener() {
                public void actionPerformed(ActionEvent e){
-                    jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set default close operation
-                    jframe.setSize(300, 200);
-                    jframe.setLocationRelativeTo(null);
                     jframe.dispose();
 
                     roomsListController.getAllRooms();

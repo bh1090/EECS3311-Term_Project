9856ef065472b19ccef1643d70c674595b55a686
George <george_zakko@hotmail.com>
2025-11-23 17:07:52 -0500

    Uncommented this code because it instantiates and declares the necessary object of the UserService class.

--- src/main/java/Controller/ChiefEventCoordinatorController.java
+++ src/main/java/Controller/ChiefEventCoordinatorController.java
@@ -1,10 +1,10 @@
 package Controller;
 
 public class ChiefEventCoordinatorController {
-//     final UserService userService;
-//     public ChiefEventCoordinatorController(UserService userService){
-//          this.userService = userService;
-//     }
+     final UserService userService;
+     public ChiefEventCoordinatorController(UserService userService){
+          this.userService = userService;
+     }
      public void createAdmin(String newAdminName, String newAdminEmail, String newAdminPassword) {
           // userService.createUser(accountType.STAFF, newAdminName, newAdminEmail, newAdminPassword);
 

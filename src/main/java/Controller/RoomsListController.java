c56057d9e98f3435704d450c5a2e67a5b59caef4
George <george_zakko@hotmail.com>
2025-11-23 01:28:02 -0500

    Uncommented code for RoomService object creation. Added import for RoomService class.

--- src/main/java/Controller/RoomsListController.java
+++ src/main/java/Controller/RoomsListController.java
@@ -1,10 +1,12 @@
 package Controller;
 
+import core.service.RoomService;
+
 public class RoomsListController {
-//     final RoomService roomService;
-//     public RoomsListController(RoomService roomService){
-//          this.roomService = roomService;
-//     }
+     final RoomService roomService;
+     public RoomsListController(RoomService roomService){
+          this.roomService = roomService;
+     }
      public void getAllRooms() {
           // roomService.getRooms();
      }

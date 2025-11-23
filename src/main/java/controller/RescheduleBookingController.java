b00e99a42e8f47a6ca280200471e6e2799bd6acd
Oscar Zhou <racso101@my.yorku.ca>
2025-11-22 20:57:17 -0500

    a2

--- src/main/java/controller/RescheduleBookingController.java
+++ src/main/java/controller/RescheduleBookingController.java
@@ -1,9 +1,26 @@
 package controller;
 //import service.RoomService;
 
+import service.RoomService;
+import view.RescheduleBookingView;
+
 public class RescheduleBookingController {
 	private RescheduleBookingView view;
-	private service;
+	private RoomService service;
 	
+	public RescheduleBookingController(RescheduleBookingView view, RoomService service) {
+		this.view = view;
+		this.service = service;
+		
+		this.view.backButton.addActionListener(e -> back());
+		this.view.rescheduleBookingButton.addActionListener(e -> back());
+	}
 	
+	public void back() {
+		
+	}
+	
+	public void rescheduleBooking() {
+		
+	}
 }
--- src/main/java/controller/RoomBookingRequirementsController.java
+++ src/main/java/controller/RoomBookingRequirementsController.java
@@ -1,5 +1,26 @@
 package controller;
 
-public class RoomBookingRequirementsController {
+import service.RoomService;
+import view.RoomBookingRequirementsView;
 
+public class RoomBookingRequirementsController {
+	private RoomBookingRequirementsView view;
+	private RoomService service;
+	
+	public RoomBookingRequirementsController(RoomBookingRequirementsView view, RoomService service) {
+		this.view = view;
+		this.service = service;
+		
+		this.view.backButton.addActionListener(e -> back());
+		this.view.bookRoomButton.addActionListener(e -> bookRoom());
+	}
+
+	private void bookRoom() {
+		
+	}
+
+	private void back() {
+		this.view.dispose();
+		//back
+	}
 }

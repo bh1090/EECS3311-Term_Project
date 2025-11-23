2241a6c3b4d1c4750ee6870bb48cc62116739c1f
George <george_zakko@hotmail.com>
2025-11-22 21:48:44 -0500

    Helper method to confirm validity of email through pattern matching.

--- src/main/java/View/ChiefEventCoordinatorView.java
+++ src/main/java/View/ChiefEventCoordinatorView.java
@@ -87,4 +87,8 @@ public class ChiefEventCoordinatorView {
                }
           });
      }
+     private boolean isValidEmail(String email) {
+          if (email == null) return false;
+          return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+     }
 }

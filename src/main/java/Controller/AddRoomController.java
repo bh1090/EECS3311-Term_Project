d387dc58297165330e1558b7dc6ade0b9e336561
George <george_zakko@hotmail.com>
2025-11-23 17:02:08 -0500

    Just parameter's passed into the createRoom method to match method signature in the RoomService class.

--- src/main/java/Controller/AddRoomController.java
+++ src/main/java/Controller/AddRoomController.java
@@ -8,6 +8,6 @@ public class AddRoomController {
           this.roomService = roomService;
      }
      public void handleAddRoomSubmission(String location, int capacity, double price){
-          // roomService.createRoom(location, capacity, price);
+          roomService.createRoom(location, price, String.valueOf(capacity));
      }
 }

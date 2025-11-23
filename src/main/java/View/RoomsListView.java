27ddee5291a42cce27f424ff4598d0cb78b09f30
George <george_zakko@hotmail.com>
2025-11-22 22:33:33 -0500

    Added private modifier.

--- src/main/java/View/RoomsListView.java
+++ src/main/java/View/RoomsListView.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RoomsListView {
-     final RoomsListController roomsListController;
+     private final RoomsListController roomsListController;
 
      public RoomsListView(RoomsListController roomsListController) {
           this.roomsListController = roomsListController;

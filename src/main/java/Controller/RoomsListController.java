58708588479e6ddcf5cad17408a71419b059dbba
George <george_zakko@hotmail.com>
2025-11-23 12:50:59 -0500

    Uncommented code for RoomService object creation.

--- src/main/java/Controller/RoomsListController.java
+++ src/main/java/Controller/RoomsListController.java
@@ -1,10 +1,10 @@
 package Controller;
 
 public class RoomsListController {
-//     final RoomService roomService;
-//     public RoomsListController(RoomService roomService){
-//          this.roomService = roomService;
-//     }
+     final RoomService roomService;
+     public RoomsListController(RoomService roomService){
+          this.roomService = roomService;
+     }
      public void getAllRooms() {
           // roomService.getRooms();
      }

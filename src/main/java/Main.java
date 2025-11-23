7aca7cc682f77050f5c02d82e49ee578aafc4c46
George <george_zakko@hotmail.com>
2025-11-22 22:52:08 -0500

    Just have everything commented out here; used this class for testing.  Not actually important.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import View.ChiefEventCoordinatorView;
 
 public class Main {
      public static void main(String[] args){
-          AdminSelectActionView adminSelectActionView = new AdminSelectActionView();
-          adminSelectActionView.handleAdminAction();
+//          AdminSelectActionView adminSelectActionView = new AdminSelectActionView();
+//          adminSelectActionView.handleAdminAction();
 
 //          ChiefEventCoordinatorView chiefEventCoordinatorView = new ChiefEventCoordinatorView();
 //          chiefEventCoordinatorView.handleChiefCoordinatorAction();

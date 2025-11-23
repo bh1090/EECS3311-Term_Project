542b17915b631778055ad317838aec064f3c43a1
George <george_zakko@hotmail.com>
2025-11-22 22:52:18 -0500

    Context comment.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import View.AdminSelectActionView;
 import View.ChiefEventCoordinatorView;
 
+// Just have everything commented out here; used this class for testing.  Not actually important.
 public class Main {
      public static void main(String[] args){
 //          AdminSelectActionView adminSelectActionView = new AdminSelectActionView();

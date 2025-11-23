8df3732c0b18a2e5dd4ba1ab93e8f7fb9049bbab
George <george_zakko@hotmail.com>
2025-11-23 17:35:52 -0500

    Uncommented out code to properly declare an object of type RoomService and initialize it via the class constructor.

--- src/main/java/Controller/RoomsListController.java
+++ src/main/java/Controller/RoomsListController.java
@@ -3,10 +3,10 @@ package Controller;
 import Service.RoomService;
 
 public class RoomsListController {
-//     final RoomService roomService;
-//     public RoomsListController(RoomService roomService){
-//          this.roomService = roomService;
-//     }
+     final RoomService roomService;
+     public RoomsListController(RoomService roomService){
+          this.roomService = roomService;
+     }
      public void getAllRooms() {
           // roomService.getRooms();
      }

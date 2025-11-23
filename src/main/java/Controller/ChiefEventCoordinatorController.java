d407f45feccfaaf1d236e3e4944b387a132520a3
George <george_zakko@hotmail.com>
2025-11-23 01:25:08 -0500

    Uncommented code for UserService object creation. Added import for UserService class/

--- src/main/java/Controller/ChiefEventCoordinatorController.java
+++ src/main/java/Controller/ChiefEventCoordinatorController.java
@@ -1,10 +1,12 @@
 package Controller;
 
+import Service.UserService;
+
 public class ChiefEventCoordinatorController {
-//     final UserService userService;
-//     public ChiefEventCoordinatorController(UserService userService){
-//          this.userService = userService;
-//     }
+     final UserService userService;
+     public ChiefEventCoordinatorController(UserService userService){
+          this.userService = userService;
+     }
      public void createAdmin(String newAdminName, String newAdminEmail, String newAdminPassword) {
           // userService.createUser(accountType.STAFF, newAdminName, newAdminEmail, newAdminPassword);
 

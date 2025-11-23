f70940505358ed9918f45c32dffb463765c3ae5b
George <george_zakko@hotmail.com>
2025-11-23 17:01:31 -0500

    Uncommented out code for RoomService object creation and initializtion through class constructor.

--- src/main/java/Controller/AddRoomController.java
+++ src/main/java/Controller/AddRoomController.java
@@ -3,10 +3,10 @@ package Controller;
 import Service.RoomService;
 
 public class AddRoomController {
-//     final RoomService roomService;
-//     public AddRoomController(RoomService roomService){
-//          this.roomService = roomService;
-//     }
+     final RoomService roomService;
+     public AddRoomController(RoomService roomService){
+          this.roomService = roomService;
+     }
      public void handleAddRoomSubmission(String location, int capacity, double price){
           // roomService.createRoom(location, capacity, price);
      }

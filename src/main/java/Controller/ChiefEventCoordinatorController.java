d18916705dbab2063a02aea3ef9be39a519bd3bb
George <george_zakko@hotmail.com>
2025-11-23 12:35:04 -0500

    Added a private modifier to the userService object, and have the class constructor initializing it.

--- src/main/java/Controller/ChiefEventCoordinatorController.java
+++ src/main/java/Controller/ChiefEventCoordinatorController.java
@@ -1,10 +1,10 @@
 package Controller;
 
 public class ChiefEventCoordinatorController {
-//     final UserService userService;
-//     public ChiefEventCoordinatorController(UserService userService){
-//          this.userService = userService;
-//     }
+     private final UserService userService;
+     public ChiefEventCoordinatorController(UserService userService, AccountType accountType){
+          this.userService = userService;
+     }
      public void createAdmin(String newAdminName, String newAdminEmail, String newAdminPassword) {
           // userService.createUser(accountType.STAFF, newAdminName, newAdminEmail, newAdminPassword);
 

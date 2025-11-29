7372e5817303139fe09276a63ced200e5b765eee
Oscar Zhou <racso101@my.yorku.ca>
2025-11-29 07:20:43 -0500

    make oscar.java a suite

--- src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/Oscar/Oscar.java
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/Oscar/Oscar.java
@@ -1,6 +1,27 @@
 package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.Oscar;
 
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+    AddSensorControllerTest.class,
+    CancelledStateTest.class,
+    CheckinControllerTest.class,
+    CheckInStateTest.class,
+    CheckoutControllerTest.class,
+    GuestSelectActionControllerTest.class,
+    OccupancySensorDataTest.class,
+    OccupancySensorTest.class,
+    PaymentViewControllerTest.class,
+    RescheduleBookingControllerTest.class,
+    RoomBookingRequirementsControllerTest.class,
+    ScanIDSensorDataTest.class,
+    ScanIDSensorTest.class,
+    SensorServiceTest.class,
+    SensorsListControllerTest.class,
+})
+
 public class Oscar {
-	
 }
 

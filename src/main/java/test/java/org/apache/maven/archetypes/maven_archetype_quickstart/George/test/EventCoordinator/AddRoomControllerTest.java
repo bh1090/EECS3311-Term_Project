06feebec279a9d40eb336f98452d6b82f7e30f4d
George <george_zakko@hotmail.com>
2025-11-30 10:34:54 -0500

    Created the first 7 test cases for the AddRoomController class.

--- src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/EventCoordinator/AddRoomControllerTest.java
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/EventCoordinator/AddRoomControllerTest.java
@@ -2,4 +2,20 @@ package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.George.
 
 public class AddRoomControllerTest {
 
+          Assertions.assertEquals(actualRoomCapacity, expectedRoomCapacity, "The room's capacity is incorrect.  ");
+     }
+     @Test
+     public void checkRoomPriceTest(){
+          Room room = RoomRepository.getInstance().findById("1"); // Assuming this method call has already been tested.
+          double expectedRoomPrice = room.getPrice();
+          double actualRoomPrice = 20;
+
+          Assertions.assertEquals(actualRoomPrice, expectedRoomPrice, "The room's price is incorrect.  ");
+     }
+     @Test
+     public void roomExistsInRoomRepositoryTest(){
+          Room room = RoomRepository.getInstance().findById("1"); // Assuming this method call has already been tested.
+
+          Assertions.assertNotNull(room, "The room wasn't properly stored in the room repository.  ");
+     }
 }

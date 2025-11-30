0f0868ff7a56fc3fd903a92f2eceee28ec8a848a
George <george_zakko@hotmail.com>
2025-11-30 11:22:53 -0500

    Added necessary imports and created the setUp method for the AdminSelectActionControllerTest class.

--- src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/EventCoordinator/AdminSelectActionControllerTest.java
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/EventCoordinator/AdminSelectActionControllerTest.java
@@ -1,5 +1,18 @@
 package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.George.test.EventCoordinator;
 
+import Controller.AddRoomController;
+import Controller.AdminSelectActionController;
+import Service.RoomService;
+import org.junit.jupiter.api.BeforeEach;
+
 public class AdminSelectActionControllerTest {
+     private AdminSelectActionController adminSelectActionController;
+
+     @BeforeEach
+     public void setUp(){
+          RoomService roomService = new RoomService();
+          AddRoomController addRoomController = new AddRoomController(roomService);
+          adminSelectActionController = new AdminSelectActionController(addRoomController);
+     }
 
 }

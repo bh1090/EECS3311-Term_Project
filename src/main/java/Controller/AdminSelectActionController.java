23fc9707c541b96d44f2fe64ad5b8d91145af958
George <george_zakko@hotmail.com>
2025-11-30 10:54:33 -0500

    Deleted anything that had to do with accessing a list of all the rooms in a view to reduce the complexity. Req6 only requires an admin be able to add a room, enable a room, or a disable a room.

--- src/main/java/Controller/AdminSelectActionController.java
+++ src/main/java/Controller/AdminSelectActionController.java
@@ -6,19 +6,12 @@ import View.RoomsListView;
 
 public class AdminSelectActionController {
      private final AddRoomController addRoomController;
-     private final RoomsListController roomsListController;
      public AdminSelectActionController(AddRoomController addRoomController, RoomsListController roomsListController) {
           this.addRoomController = addRoomController;
-          this.roomsListController = roomsListController;
      }
-
      public void accessAddRoomView() {
           AddRoomView addRoomView = new AddRoomView(addRoomController);
           addRoomView.getAdminAddRoomInput();
-     }
-     public void accessRoomsListView() {
-          RoomsListView roomsListView = new RoomsListView(roomsListController);
-          roomsListView.getListOfRooms();
      }
      public void enableRoom(String roomID) {
           RoomService roomService = new RoomService();

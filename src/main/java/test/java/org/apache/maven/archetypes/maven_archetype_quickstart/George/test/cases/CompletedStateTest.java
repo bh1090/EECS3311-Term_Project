1007ae7714497bc6346beccecf642ed1c3f08192
George <george_zakko@hotmail.com>
2025-11-30 19:26:26 -0500

    Added 4 more tests that test the checkIn, checkOut, and cancel methods and also check if the CompletedState object is not null. Also updated the setUp method so it creates a booking object before all the tests run. Inconsequential changes are regarding imports and formatting.

--- src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/cases/CompletedStateTest.java
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/cases/CompletedStateTest.java
@@ -1,23 +1,61 @@
 package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.George.test.cases;
 
+import Model.Room.Booking;
 import Model.State.BookingState;
 import Model.State.CompletedState;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public class CompletedStateTest {
      private static BookingState bookingState;
-
+     private static Booking booking;
      @BeforeAll
      public static void setUp(){
           bookingState = new CompletedState();
+          Booking booking = new Booking("1", "1", "1",
+                  LocalDate.of(2030, 12, 12),
+                  LocalDate.of(2030, 12, 13),
+                  LocalTime.of(12, 0),
+                  LocalTime.of(13, 0),
+                  "1");
      }
 
      @Test
+     public void objectCompletedStateNotNull(){
+
+          Assertions.assertNotNull(bookingState, "Object of CompletedState is null.  ");
+     }
+     @Test
      public void completedStateNameTest(){
+          String expectedStateName = bookingState.getStatus();
+          String actualStateName = "COMPLETED";
+
+          Assertions.assertEquals(actualStateName, expectedStateName, "State's aren't the same.  ");
+     }
+     @Test
+     public void completedStateCancelTest(){
+          bookingState.cancel(booking);
+          String expectedStateName = bookingState.getStatus();
+          String actualStateName = "COMPLETED";
+
+          Assertions.assertEquals(actualStateName, expectedStateName, "State's aren't the same.  ");
+     }
+     @Test
+     public void completedStateCheckInTest(){
+          bookingState.checkIn(booking);
+          String expectedStateName = bookingState.getStatus();
+          String actualStateName = "COMPLETED";
+
+          Assertions.assertEquals(actualStateName, expectedStateName, "State's aren't the same.  ");
+     }
+     @Test
+     public void completedStateCheckOutTest(){
+          bookingState.checkOut(booking);
           String expectedStateName = bookingState.getStatus();
           String actualStateName = "COMPLETED";
 

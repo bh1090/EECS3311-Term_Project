bbce1f69e87be600ffc2b04d9d4d682c9e8b1fe9
George <george_zakko@hotmail.com>
2025-11-30 21:12:35 -0500

    Created the RoomDisabledStateTest class with a basic setUp() method.

---
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/cases/RoomDisabledStateTest.java
@@ -0,0 +1,14 @@
+package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.George.test.cases;
+
+import Model.Room.Room;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+
+public class RoomDisabledStateTest {
+     private Room room;
+
+     @BeforeAll
+     public static void setUp(){
+
+     }
+}

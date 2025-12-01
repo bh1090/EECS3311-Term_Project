aaabaf457f680ad58d72f62b5087aff9fede4998
George <george_zakko@hotmail.com>
2025-11-30 21:28:03 -0500

    Added changes to the setUp method so it also sets the roomState and room object's state to be RoomDisabledState. Also wrote first skeleton of a test.

--- src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/cases/RoomDisabledStateTest.java
+++ src/main/java/test/java/org/apache/maven/archetypes/maven_archetype_quickstart/George/test/cases/RoomDisabledStateTest.java
@@ -1,14 +1,25 @@
 package test.java.org.apache.maven.archetypes.maven_archetype_quickstart.George.test.cases;
 
 import Model.Room.Room;
+import Model.State.RoomDisabledState;
+import Model.State.RoomState;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class RoomDisabledStateTest {
-     private Room room;
+     private static RoomState roomState;
+     private static Room room;
 
      @BeforeAll
      public static void setUp(){
+          room = new Room("1", "Lassonde 1111", 50, "12");
+          roomState = new RoomDisabledState();
+          room.setState(new RoomDisabledState());
+     }
+
+     @Test
+     public void Test(){
 
      }
 }
